43e9780fcfea6a841b35ad9c9d2d34f997d3bf77
Michal <michal@mloza.pl>
2014-11-08 14:16:47 +0100

    initial commit

---
+++ module/src/main/java/pl/agh/edu/BridgeServer.java
@@ -0,0 +1,43 @@
+package pl.agh.edu;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.sockjs.SockJSServer;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * Created by Michal
+ * 2014-11-08.
+ */
+public class BridgeServer extends Verticle {
+    Logger logger;
+
+    public void start() {
+        logger = container.logger();
+
+        HttpServer server = vertx.createHttpServer();
+
+        // Also serve the static resources. In real life this would probably be done by a CDN
+        server.requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                if (req.path().equals("/")) req.response().sendFile("index.html"); // Serve the index.html
+                if (req.path().endsWith("vertxbus.js")) req.response().sendFile("vertxbus-2.1.js"); // Serve the js
+            }
+        });
+
+        JsonArray permitted = new JsonArray();
+        permitted.add(new JsonObject()); // Let everything through
+
+        ServerHook hook = new ServerHook(logger);
+
+        SockJSServer sockJSServer = vertx.createSockJSServer(server);
+        sockJSServer.setHook(hook);
+        sockJSServer.bridge(new JsonObject().putString("prefix", "/eventbus"), permitted, permitted);
+
+        server.listen(8888);
+    }
+}
