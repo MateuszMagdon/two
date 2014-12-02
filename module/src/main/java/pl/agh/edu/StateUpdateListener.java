09875513722c85c16162ff5b2859b32b54e742cd
Marek <Marek@Whatever.local>
2014-12-02 17:35:22 +0100

    Broadcast game state using prepared classes from model

--- module/src/main/java/pl/agh/edu/StateUpdateListener.java
+++ module/src/main/java/pl/agh/edu/StateUpdateListener.java
@@ -7,6 +7,9 @@ import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
+import com.google.gson.Gson;
+
+import pl.agh.edu.model.ChangeRequest;
 import pl.agh.edu.model.Game;
 
 public class StateUpdateListener extends Verticle{
@@ -16,20 +19,14 @@ public class StateUpdateListener extends Verticle{
 	EventBus eb;
 	Game game;
 	
-	private synchronized void updateModel(JsonObject update){
-		//unpack update
+	private synchronized void updateModel(ChangeRequest update){
 		//do updates on game object
 	}
 	
 	
-	private synchronized void broadcastModel(){
-		
-		//JsonObject gameJSON = game.toJson()
-		JsonObject gameJSON = new JsonObject();
+	private synchronized void broadcastModel(JsonObject gameJSON){
 		eb.publish("two.clients", gameJSON);
 	}
-	
-	
 	
     public void start() {
         logger = container.logger();
@@ -38,9 +35,12 @@ public class StateUpdateListener extends Verticle{
         //handler to receive updates from clients
         Handler<Message<JsonObject>> updateHandler = new Handler<Message<JsonObject>>() {
         	public void handle(Message<JsonObject> message) {
-        		logger.info("Received update from: ");
-        		updateModel(message.body());
-        		logger.info("Updated model from client: ");
+        		JsonObject json = message.body();
+        		//TO DO unpack change from json
+        		ChangeRequest change = new ChangeRequest(1,1,true);
+        		
+        		logger.info("Received update from player: " + change.getPlayerId());
+        		updateModel(change);
             }
         };
         eb.registerHandler("two.server", updateHandler);
@@ -49,7 +49,7 @@ public class StateUpdateListener extends Verticle{
         //periodic task to broadcast game state every interval
         long timerID = vertx.setPeriodic(1200, new Handler<Long>() {
             public void handle(Long timerID) {
-            	broadcastModel();
+            	broadcastModel(game.toJson());
             }
         });
         logger.info("TimerID is: " + timerID);
