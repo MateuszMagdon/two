6e24077ac1806648dd82d63618900f5bc07b21a1
Marek <Marek@Whatever.local>
2014-12-05 18:06:28 +0100

    Przeniesiono obsluge zmian do wierzcholka symulacji

--- module/src/main/java/pl/agh/edu/Simulator.java
+++ module/src/main/java/pl/agh/edu/Simulator.java
@@ -1,12 +1,23 @@
 package pl.agh.edu;
 
-import com.google.common.collect.ImmutableList;
+import java.util.concurrent.ConcurrentMap;
+
+import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
-import pl.agh.edu.model.*;
 
-import java.util.concurrent.ConcurrentMap;
+import pl.agh.edu.model.Bullet;
+import pl.agh.edu.model.ChangeRequest;
+import pl.agh.edu.model.Game;
+import pl.agh.edu.model.Plane;
+import pl.agh.edu.model.PlaneTypes;
+import pl.agh.edu.model.Player;
+
+import com.google.common.collect.ImmutableList;
+import com.google.gson.Gson;
 
 /**
  * Created by Michal
@@ -20,6 +31,7 @@ public class Simulator extends Verticle {
     private EventBus eb;
     private Boolean continueGame = true;
     Long time = System.currentTimeMillis();
+    private static final Gson GSON = new Gson();
 
     public Plane createSimplePlane() {
         return new Plane(PlaneTypes.STANDARD.getPlaneType(), 0, 0, 45, 10.f, null, 10, false, System.currentTimeMillis(), null);
@@ -63,12 +75,35 @@ public class Simulator extends Verticle {
         });
         eb.publish("game.start", false);
 
-        getVertx().eventBus().registerHandler("user.connect", message -> {
+        eb.registerHandler("user.connect", message -> {
 
         });
 
-        getVertx().eventBus().registerHandler("user.disconnect", message -> {
+        eb.registerHandler("user.disconnect", message -> {
 
         });
+        
+        
+        
+        //handler to receive updates from clients
+        eb.registerHandler("two.server", new Handler<Message<JsonObject>>() {
+        	public void handle(Message<JsonObject> message) {
+        		JsonObject json = message.body();
+        		ChangeRequest change = GSON.fromJson(json.toString(), ChangeRequest.class);
+        		logger.debug("Update from player: " + change.getPlayerId() + "appended.");
+        		
+        		//TO DO aply changeRequest to createNewGame    		
+            }
+        });
+        
+        
+        //periodic task to broadcast game state every interval
+        vertx.setPeriodic(1200, new Handler<Long>() {
+            public void handle(Long timerID) {
+        		eb.publish("two.clients", game.toJson());
+            }
+        });
+        
+        
     }
 }
