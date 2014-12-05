ed34ac85f16a4abc5f1f84f595dc07f8845a4610
Marek <Marek@Whatever.local>
2014-12-05 15:57:50 +0100

    Przekazywanie ChangeRequestów do symulacji i broadcast

--- module/src/main/java/pl/agh/edu/StateUpdateListener.java
+++ module/src/main/java/pl/agh/edu/StateUpdateListener.java
@@ -1,5 +1,7 @@
 package pl.agh.edu;
 
+import java.util.concurrent.ConcurrentMap;
+
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.eventbus.Message;
@@ -7,40 +9,33 @@ import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
-import com.google.gson.Gson;
-
 import pl.agh.edu.model.ChangeRequest;
 import pl.agh.edu.model.Game;
+
+import com.google.gson.Gson;
 
 public class StateUpdateListener extends Verticle{
 
 	
 	Logger logger;
 	EventBus eb;
-	Game game;
+	ConcurrentMap<String, Game> map;
+	private static final Gson GSON = new Gson();
 	
-	private synchronized void updateModel(ChangeRequest update){
-		//do updates on game object
-	}
-	
-	
-	private synchronized void broadcastModel(JsonObject gameJSON){
-		eb.publish("two.clients", gameJSON);
-	}
 	
     public void start() {
         logger = container.logger();
         eb = vertx.eventBus();        
+        map =  vertx.sharedData().getMap("shared");
+        
         
         //handler to receive updates from clients
         Handler<Message<JsonObject>> updateHandler = new Handler<Message<JsonObject>>() {
         	public void handle(Message<JsonObject> message) {
         		JsonObject json = message.body();
-        		//TO DO unpack change from json
-        		ChangeRequest change = new ChangeRequest(1,1,true);
-        		
-        		logger.info("Received update from player: " + change.getPlayerId());
-        		updateModel(change);
+        		ChangeRequest change = GSON.fromJson(json.toString(), ChangeRequest.class);
+        		Simulator.changeRequestQueue.add(change);
+        		logger.info("Update from player: " + change.getPlayerId() + "appended.");
             }
         };
         eb.registerHandler("two.server", updateHandler);
@@ -49,7 +44,8 @@ public class StateUpdateListener extends Verticle{
         //periodic task to broadcast game state every interval
         long timerID = vertx.setPeriodic(1200, new Handler<Long>() {
             public void handle(Long timerID) {
-            	broadcastModel(game.toJson());
+        		Game game = map.get("game");
+        		eb.publish("two.clients", game.toJson());
             }
         });
         logger.info("TimerID is: " + timerID);
